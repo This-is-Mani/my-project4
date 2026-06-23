042f7691904823806b7552eb054834a9be38c2d0
This-is-Mani <207173292+This-is-Mani@users.noreply.github.com>
2026-06-23 09:12:49 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React CI/CD on AWS S3!</h1>
+      <p>This React app was built entirely by GitHub Actions.</p>
+    </div>
+  );
+}
+
+export default App;
